508d8e212946161e748600d53a0f0ceca8e1276a
Eli Young <elyscape@gmail.com>
2019-11-12 15:14:36 -0800

    Build fyde-cli from source

--- Formula/fyde-cli.rb
+++ Formula/fyde-cli.rb
@@ -1,29 +1,39 @@
 class FydeCli < Formula
   desc "Fyde Enterprise Console command-line tool"
   homepage "https://github.com/fyde/fyde-cli"
-  version "0.4"
-  bottle :unneeded
+  url "https://github.com/fyde/fyde-cli.git",
+      :tag      => "v0.4",
+      :revision => "d147fbb9e081e9fd6f4ad9992679c52a813a0a54"
 
-  if OS.mac?
-    url "https://github.com/fyde/fyde-cli/releases/download/v0.4/fyde-cli_v0.4_macOS-amd64.tar.gz"
-    sha256 "77930609cfa80c5d77d058749634ea48a056f7faf1d099598a7ca000d284eb5a"
-  elsif OS.linux?
-    if Hardware::CPU.intel?
-      url "https://github.com/fyde/fyde-cli/releases/download/v0.4/fyde-cli_v0.4_linux-amd64.tar.gz"
-      sha256 "da0238ba6655714e14e8fd6053505211eb6b3c5010a28e515abc5e463399ea8c"
-    elsif Hardware::CPU.arm?
-      if Hardware::CPU.is_64_bit?
-        url "https://github.com/fyde/fyde-cli/releases/download/v0.4/fyde-cli_v0.4_linux-arm64.tar.gz"
-        sha256 "eff51326685362437052e028a887cfc3338e64fadd156b4545b9bf5871eb02c6"
-      else
-        url "https://github.com/fyde/fyde-cli/releases/download/v0.4/fyde-cli_v0.4_linux-arm.tar.gz"
-        sha256 "595654208b98c83e19068452d1bf874d1ecc76fd6ca1c1804e4bda56a721c97f"
-      end
-    end
+  depends_on "go" => :build
+
+  resource "go-swagger" do
+    url "https://github.com/go-swagger/go-swagger.git",
+        :tag => "v0.21.0"
+  end
+
+  resource "govvv" do
+    url "https://github.com/ahmetb/govvv.git",
+        :revision => "eeed55f64b034cbb8d82676030f98676e1b4dae1"
   end
 
   def install
-    bin.install "fyde-cli"
+    home_bindir = Pathname.new(ENV["HOME"])/"bin"
+    ENV.prepend_create_path "PATH", home_bindir
+
+    resource("go-swagger").stage do
+      system "go", "build", "-o", home_bindir/"swagger", "./cmd/swagger"
+    end
+
+    resource("govvv").stage do
+      system "go", "build", "-o", home_bindir/"govvv"
+    end
+
+    system "git", "checkout", "."
+    (buildpath/".git/info/exclude").append_lines(".brew_home/")
+
+    system "swagger", "generate", "client", "-f", "swagger.yml"
+    system "govvv", "build", "-version", version, "-o", "#{bin}/fyde-cli"
 
     # Install bash completion
     output = Utils.popen_read("#{bin}/fyde-cli completion bash")
@@ -32,6 +42,9 @@ class FydeCli < Formula
     # Install zsh completion
     output = Utils.popen_read("#{bin}/fyde-cli completion zsh")
     (zsh_completion/"_fyde-cli").write output
+
+    # Clean up build path
+    system "go", "clean", "-modcache"
   end
 
   test do
