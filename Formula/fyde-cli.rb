4325ba7c4633ad67169c4113c1b9a893f8c05fc4
Eli Young <elyscape@gmail.com>
2019-11-15 17:29:37 -0800

    fyde-cli: Update to 0.4.1

--- Formula/fyde-cli.rb
+++ Formula/fyde-cli.rb
@@ -1,23 +1,23 @@
 class FydeCli < Formula
   desc "Fyde Enterprise Console command-line tool"
   homepage "https://github.com/fyde/fyde-cli"
-  version "0.4"
+  version "0.4.1"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/fyde/fyde-cli/releases/download/v0.4/fyde-cli_v0.4_macOS-amd64.tar.gz"
-    sha256 "77930609cfa80c5d77d058749634ea48a056f7faf1d099598a7ca000d284eb5a"
+    url "https://github.com/fyde/fyde-cli/releases/download/v0.4.1/fyde-cli_v0.4.1_macOS-amd64.tar.gz"
+    sha256 "89214ac9c1ab0b956d814ef58dd9695fa08f9a1826f044e7d4834a6e0bb9a08a"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/fyde/fyde-cli/releases/download/v0.4/fyde-cli_v0.4_linux-amd64.tar.gz"
-      sha256 "da0238ba6655714e14e8fd6053505211eb6b3c5010a28e515abc5e463399ea8c"
+      url "https://github.com/fyde/fyde-cli/releases/download/v0.4.1/fyde-cli_v0.4.1_linux-amd64.tar.gz"
+      sha256 "0362fa5f649d95be122730f2931497d88d0057fa70c7d13a2e4e99ae96474d9f"
     elsif Hardware::CPU.arm?
       if Hardware::CPU.is_64_bit?
-        url "https://github.com/fyde/fyde-cli/releases/download/v0.4/fyde-cli_v0.4_linux-arm64.tar.gz"
-        sha256 "eff51326685362437052e028a887cfc3338e64fadd156b4545b9bf5871eb02c6"
+        url "https://github.com/fyde/fyde-cli/releases/download/v0.4.1/fyde-cli_v0.4.1_linux-arm64.tar.gz"
+        sha256 "d73ba3bf32262face8ce5b5110193e0a0cbec29a39af4842df5886b5ad86f6e0"
       else
-        url "https://github.com/fyde/fyde-cli/releases/download/v0.4/fyde-cli_v0.4_linux-arm.tar.gz"
-        sha256 "595654208b98c83e19068452d1bf874d1ecc76fd6ca1c1804e4bda56a721c97f"
+        url "https://github.com/fyde/fyde-cli/releases/download/v0.4.1/fyde-cli_v0.4.1_linux-arm.tar.gz"
+        sha256 "ae4ef05d5b1e45c0bd555b7102d962b292dd1ed0acf1787d405f53f79e72fe47"
       end
     end
   end
